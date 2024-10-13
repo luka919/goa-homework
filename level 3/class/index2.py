c50fbc187b86b7938af93d49f6facabef7a29e78
luka919 <r.abashidze007@gmail.com>
2024-10-13 16:53:13 +0400

    commit

---
+++ level 3/class/index2.py
@@ -0,0 +1,11 @@
+name = "luka"
+last_name = "miqeladze"
+age = 11
+country = "georgia"
+favorite_car = "mercedes"
+fevorite_color = "blue"
+
+print("my name is"  + name + "  " + "my last_name is" + "  " + last_name + "  " +  "i live in" + "  " + country + "  " + "my favorite_car is" + "  " + favorite_car + "  " + "my favorite_color is" + "  " + fevorite_color)
+
+
+print(f"my name is {name} my last_name is {last_name} i live in {country} my fevorite_car is {favorite_car} my fevorite_color is {fevorite_color}")
