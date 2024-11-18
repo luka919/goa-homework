523486e952035e4b3ac7798a807bf4cf36cfa3bd
luka919 <r.abashidze007@gmail.com>
2024-11-18 22:31:12 +0400

    commit

---
+++ level 22/home/index.py
@@ -0,0 +1,6 @@
+def greeting(age):
+    if age <= 18:
+        print('you get a descuont')
+    else:
+        print('you dont get a descuont')
+greeting(18)
---
+++ level 22/home/index1.py
@@ -0,0 +1,4 @@
+def manual_reverse(s):
+    return s[::-1]
+
+print(manual_reverse('i love goa'))
