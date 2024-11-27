cb18cc6d9649eb83dc2d2d15246d1774960c45c0
luka919 <r.abashidze007@gmail.com>
2024-11-27 19:58:32 +0400

    commit

---
+++ level 30/home/index1.py
@@ -0,0 +1,3 @@
+def no_space(x):
+    return x.replace('o','hello world')
+print(no_space("hello world"))
---
+++ level 30/home/index2.py
@@ -0,0 +1,7 @@
+def manual_len(x):
+    count = 0
+    for i in x:
+        count += 1
+    return count
+about_me = "i am luka i am 11 years old"
+print(manual_len(about_me))
