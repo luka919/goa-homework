579287e35bee89aab13f8d95be703c1870895417
luka919 <r.abashidze007@gmail.com>
2025-02-09 12:44:28 +0400

    commit

---
+++ level 56/home/index.py
@@ -0,0 +1,6 @@
+# codewars
+# 1) https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/python
+# 2) https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/python
+# 3) https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/python
+# 4) https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/python
+# 5) https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/python
