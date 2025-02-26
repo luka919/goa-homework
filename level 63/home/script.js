0a6bff0a174fcb4ab6f4990746195ca2553e0d71
luka919 <r.abashidze007@gmail.com>
2025-02-26 23:50:44 +0400

    commit

---
+++ level 63/home/script.js
@@ -0,0 +1,29 @@
+// 1)
+let i = 1;
+
+while (i <= 200) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+// 2)
+let x = 51;
+
+while (x <= 150) {
+    if (x % 3 === 0) {
+        console.log(x);
+    }
+    x++;
+}
+
+// 3)
+for (let y = 0; y <= 100; y++) {
+    console.log(y);
+}
+
+// 4)
+for (let b = 100; b <= 300; b += 2) {
+    console.log(b);
+}
