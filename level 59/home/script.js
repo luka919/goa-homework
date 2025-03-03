b0853f14792c5c882f1797bab817ba3d2b47a4c9
luka919 <r.abashidze007@gmail.com>
2025-03-03 23:07:57 +0400

    commit

---
+++ level 59/home/script.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("enter num:"))
+let num2 = Number(prompt("enter num:"))
+let num3 = Number(prompt("enter num:"))
+console.log(num1 + num2 + num3)
+
+
+let str1 = (prompt("enter word:"))
+let str2 = (prompt("enter word:"))
+
+console.log(str1 + str2)
+
+// ფორმის მონაცემების გადაცემა – როდესაც ფორმას ვაგზავნით submit ღილაკზე დაჭერისას name attribute-ა საჭიროა, რომ ბრაუზერმა და სერვერმა ამოცნოს მისი მნიშვნელობა.
